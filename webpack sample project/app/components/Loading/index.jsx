4f23e8a6c3bce206277b2e8f52341aef9d7706c6
maokailu <1113901189@qq.com>
2017-12-11 08:06:39 +0800

    Loading

---
+++ webpack sample project/app/components/Loading/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Pip from './pip.jsx';
+export default function Loading(props){
+    var nums = [0,1,2,3,4,5,6,7,8,9,10,11];
+    return(
+        
+        <div className="loading">
+            {
+                nums.map((num)=>
+                    <Pip key={num} num={num}/>
+                )
+            }
+        </div>
+    )
+}
---
+++ webpack sample project/app/components/Loading/pip.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './style.scss';
+export default function Pip(props){
+    return(
+        <div className={"pip"+props.num}></div>
+    )
+}
