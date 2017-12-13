6029c784c05a0779329d3625ee490a1ab0656f7a
maokailu <1113901189@qq.com>
2017-12-13 20:05:22 +0800

    追踪ts文件

---
+++ webpack sample project/app/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+// import Hello from './components/Hello';
+import PullView from './components/PullView';
+import './main.scss';
+
+{/* <Hello compiler="TypeScript" framework="React" /> */}
+render(<PullView />, document.getElementById('root'));
