762f0910a4f1d8d4fddafc05d72747dfee8dbf05
maokailu <1113901189@qq.com>
2017-12-21 23:14:11 +0800

    save myBlog's resource

--- webpack sample project/app/main.jsx
+++ webpack sample project/app/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import List from './website/List';
+import Nav from './myBlog/Nav';
 import './components/Adaptation';
 import './main.scss';
 
-render(<List />, document.getElementById('root'));
+render(<Nav />, document.getElementById('root'));
