86590feed22765544afae63c9bcbaa67509b563b
maokailu <1113901189@qq.com>
2017-12-30 01:38:53 +0800

    完成日历主要功能开发

--- webpack sample project/app/components/DatePicker/index.jsx
+++ webpack sample project/app/components/DatePicker/index.jsx
@@ -1,11 +1,44 @@
 import React from 'react';
+import Day from './Day';
+import SelectOption from './SelectOption';
 import '../Adaptation';
 import './style.scss';
+let date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth();
+let week = date.getDate();
+
 export default class Pagination extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      currentyear: year,
+      currentmonth: month,
+      currentIndex: this.getFirstDay(month, year) + week - 1
     };
+  }
+
+  selectYear = event => {
+    let value = parseInt(event.target.value);
+    this.setState(prevState => ({
+      currentyear: value,
+      currentIndex:
+        this.getFirstDay(prevState.currentmonth,  value) +
+        prevState.currentIndex - this.getFirstDay(prevState.currentmonth, prevState.currentyear)
+    }));
+  }
+  selectMonth = event => {
+    let value = parseInt(event.target.value);
+    this.setState(prevState => {
+      return {
+        currentmonth: value,
+        currentIndex:
+          this.getFirstDay(value,  prevState.currentyear) +
+          prevState.currentIndex - this.getFirstDay(prevState.currentmonth,  prevState.currentyear)
+      };
+    });
+  }
+  componentDidMount() {
   }
   getSizeOfMonth = (year, month) => {
     let sizeOfMonths = [31, 28, 31, 30, 31, 30, 31, 30, 31, 31, 30, 31];
@@ -15,52 +48,116 @@ export default class Pagination extends React.Component {
     let sizeOfMonth = sizeOfMonths[month];
     return sizeOfMonth;
   }
+  getFirstDay = (month, year) =>{
+    let timestamp = Date.parse((month + 1) + '/1/' + year);
+    let date = new Date(timestamp);
+    let firstDay = date.getDay();
+    return firstDay;
+  }
   getTable = () => {
-    let currentYear = 2017;let currentMonth = 5;
-    let sizeOfPrevMonth = this.getSizeOfMonth(currentYear, currentMonth - 1);
-    let sizeOfCurrentMonth = this.getSizeOfMonth(currentYear, currentMonth);
-    let timestamp = Date.parse((currentMonth + 1) + '/1/' + currentYear);
-    let date = new Date(timestamp);let week = date.getDay();
-    let prevShowDays = week;let showDays = [];
-    let table = [];
-    for (let i = sizeOfPrevMonth - prevShowDays + 1; i <= sizeOfPrevMonth; i++) {
-      showDays.push(i);
+    let sizeOfPrevMonth;
+    if (this.state.currentmonth === 0) {
+      sizeOfPrevMonth = this.getSizeOfMonth(this.state.currentyear - 1, 11);
+    } else {
+      sizeOfPrevMonth = this.getSizeOfMonth(this.state.currentyear, this.state.currentmonth - 1);
+    }
+    let sizeOfCurrentMonth = this.getSizeOfMonth(this.state.currentyear, this.state.currentmonth);
+    let firstDay = this.getFirstDay(this.state.currentmonth, this.state.currentyear);
+    let days = [];
+    for (let i = sizeOfPrevMonth - firstDay + 1; i <= sizeOfPrevMonth; i++) {
+      days.push(i);
     }
     for (let j = 1; j <= sizeOfCurrentMonth; j++) {
-      showDays.push(j);
+      days.push(j);
     }
-    for (let k = 1; k <= 42 - sizeOfCurrentMonth - week; k++) {
-      showDays.push(k);
-    }
-    for (let m = 0; m < 6; m++) {
-      let rows = [];
-      for (let n = m * 7; n <= m * 7 + 6; n++) {
-        rows.push(showDays[n]);
+    if (this.state.currentmonth !== 11 || this.state.currentyear !== year) {
+      for (let k = 1; k <= 42 - sizeOfCurrentMonth - firstDay; k++) {
+        days.push(k);
       }
-      table.push(rows);
     }
+    let table = [];
+    (new Array(6)).fill(0).forEach((row, rowIndex) => {
+      table.push(days.slice(rowIndex * 7, (rowIndex + 1) * 7));
+    });
     return table;
   }
+
+  clickHandler = index => {
+    let currentmonth = this.state.currentmonth;
+    let currentyear = this.state.currentyear;
+    let firstDayOfCurrentMonth = this.getFirstDay(currentmonth, currentyear);
+    let firstDayOfPrevMonth = this.getFirstDay(currentmonth - 1, currentyear);
+    let firstDayOfNextMonth = 0;
+    if (currentmonth === 11) {
+      firstDayOfNextMonth = this.getFirstDay(0, currentyear + 1);
+    } else {
+      firstDayOfNextMonth = this.getFirstDay(currentmonth + 1, currentyear);
+    }
+    let sizeOfCurrentMonth = this.getSizeOfMonth(currentyear, currentmonth);
+    let sizeOfPrevMonth = this.getSizeOfMonth(currentyear, currentmonth - 1);
+    this.setState({
+      currentIndex: index
+    });
+    if (index < firstDayOfCurrentMonth) {
+      this.setState(prevState => {
+        return {
+          currentmonth: prevState.currentmonth - 1,
+          currentIndex:
+            index
+            + sizeOfPrevMonth
+            - firstDayOfCurrentMonth
+            + firstDayOfPrevMonth
+        };
+      });
+    } else if (index >= firstDayOfCurrentMonth + sizeOfCurrentMonth) {
+      this.setState(prevState => ({
+        currentmonth: prevState.currentmonth === 11 ? 0 : prevState.currentmonth + 1,
+        currentyear: prevState.currentmonth === 11 ? prevState.currentyear + 1 : prevState.currentyear,
+        currentIndex:
+          index - firstDayOfCurrentMonth
+          - sizeOfCurrentMonth
+          + firstDayOfNextMonth
+      }));
+    }
+  }
+
   render() {
+    let firstDay = this.getFirstDay(this.state.currentmonth, this.state.currentyear);
+    let sizeOfCurrentMonth = this.getSizeOfMonth(this.state.currentyear, this.state.currentmonth);
     return (
       <div className="datePicker">
         <div className="fullcalender-header">
-          <div>Year</div>
-          <div>Month</div>
+          <select value = {this.state.currentyear} onChange={this.selectYear}>
+            {(new Array(5)).fill((new Date).getFullYear()).map((year, index) =>
+              <SelectOption value = {year - index} text = {year - index} key = {index} />
+            )};
+          </select>
+          <select value = {this.state.currentmonth} onChange={this.selectMonth}>
+            {(new Array(12)).fill((new Date).getMonth()).map((month, index) =>
+              <SelectOption value = {month - index} text = {month - index + 1} key = {index} />
+            )};
+          </select>
         </div>
         <table>
           <thead>
             <tr><th>日</th><th>一</th><th>二</th><th>三</th><th>四</th><th>五</th><th>六</th></tr>
           </thead>
           <tbody>
-            {this.getTable().map((day, index) =>
-              (<tr key={index}>
+            {this.getTable().map((row, rowIndex) =>
+              <tr key={rowIndex}>
                 {
-                  day.map((t, index) =>
-                    <td key={index}>{ (t >= 1 && t <= 9) ? '0' + t : t}</td>
+                  row.map((day, columnIndex) =>
+                    <Day
+                      key={rowIndex * 7 + columnIndex} index ={rowIndex * 7 + columnIndex}
+                      sizeOfPrevShowDays={firstDay}
+                      sizeOfCurrentMonth = {sizeOfCurrentMonth}
+                      day={day}
+                      clickHandler = {()=>this.clickHandler(rowIndex * 7 + columnIndex, event)}
+                      currentIndex = {this.state.currentIndex}
+                    />
                   )
                 }
-              </tr>)
+              </tr>
             )}
           </tbody>
         </table>
