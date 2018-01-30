a177f3180e6f64ce7fb0cc1bd045db9e2cc47a37
maokailu <1113901189@qq.com>
2018-01-31 02:10:10 +0800

    router

--- webpack sample project/app/main.jsx
+++ webpack sample project/app/main.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-import PullView from './components/PullView';
-// import Toast from './components/Toast';
 import './components/Adaptation';
 import './main.scss';
-
+import Home from './components/ReactRouter/router1';
 render(
-  <PullView />,
-  // <div className="btn" onClick ={()=>Toast.info()}>btn</div>,
+  <Home />,
   document.getElementById('root')
 );
--- webpack sample project/webpack.config.js
+++ webpack sample project/webpack.config.js
@@ -12,10 +12,13 @@ module.exports = {
     extensions: ['.js', '.jsx', '.json', 'ts', 'tsx']
   },
   devServer: {
+    https: true,
     contentBase: './build', // 本地服务器所加载的页面所在的目录
     historyApiFallback: true, // 不跳转
     inline: true, // 实时刷新
-    hot: true
+    hot: true,
+    // host: '192.168.1.104',
+    // port: 8090
   },
   module: {
     rules: [
