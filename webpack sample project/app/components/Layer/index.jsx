7f693d68ad4f1c4afce074360ef1e0168b5f389c
maokailu <1113901189@qq.com>
2018-03-07 15:11:08 +0800

    Modal

--- webpack sample project/app/components/Layer/index.jsx
+++ webpack sample project/app/components/Layer/index.jsx
@@ -1,43 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.scss';
 
-const modalRoot = document.createElement('modal-root');
+const modalRoot = document.createElement('modal-root'); // 容纳整个弹出层，与app层平行
 
-// Let's create a Modal component that is an abstraction around
-// the portal API.
 class Modal extends React.Component {
   constructor(props) {
     super(props);
-    // Create a div that we'll render the modal into. Because each
-    // Modal component has its own element, we can render multiple
-    // modal components into the modal container.
     this.el = document.createElement('div');
   }
 
   componentDidMount() {
-    // Append the element into the DOM on mount. We'll render
-    // into the modal container element (see the HTML tab).
     modalRoot.appendChild(this.el);
   }
 
   componentWillUnmount() {
-    // Remove the element from the DOM when we unmount
     modalRoot.removeChild(this.el);
   }
   render() {
-    // Use a portal to render the children into the element
     return ReactDOM.createPortal(
-      // Any valid React child: JSX, strings, arrays, etc.
       this.props.children,
-      // A DOM element
-      this.el,
+      this.el, // Modal的容器，可放多个Modal内容
     );
   }
 }
 
-// The Modal component is a normal React component, so we can
-// render it wherever we like without needing to know that it's
-// implemented with portals.
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -48,7 +35,6 @@ export default class App extends React.Component {
   }
 
   handleShow() {
-    // debugger
     this.setState({ showModal: true });
   }
 
@@ -57,10 +43,8 @@ export default class App extends React.Component {
   }
 
   render() {
-    // Show a Modal on click.
-    // (In a real app, don't forget to use ARIA attributes
-    // for accessibility!)
     const modal = this.state.showModal ? (
+      // Layer,将渲染到父元素app-root外与app-root平行的元素modal-root中，但在app中通过showModel控制Moal是否渲染。
       <Modal>
         <div className="modal">
           <div>
--- webpack sample project/webpack.config.js
+++ webpack sample project/webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     extensions: ['.js', '.jsx', '.json', 'ts', 'tsx']
   },
   devServer: {
-    https: true,
+    // https: true,
     contentBase: './build', // 本地服务器所加载的页面所在的目录
     historyApiFallback: true, // 不跳转
     inline: true, // 实时刷新
